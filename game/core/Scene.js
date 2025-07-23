34aaeffd4ebe010b829833ad43f3168098e488a8
Sensol2 <hhncn4471@soongsil.ac.kr>
2025-07-23 16:14:34 +0900

    Fix: Camera 렌더 리팩토링

--- game/core/Scene.js
+++ game/core/Scene.js
@@ -1,26 +1,26 @@
 class Scene {
-    constructor() {
-        this.objects = [];
-        this.uiObjects = [];
+  constructor() {
+    this.objects = [];
+    this.uiObjects = [];
+  }
+  init() {}
+  update(timestamp) {
+    for (let obj of this.objects) {
+      obj.update?.(timestamp);
     }
-    init() {}
-    update(timestamp) {
-        for (let obj of this.objects) {
-            obj.update?.(timestamp);
-        }
+  }
+  render(camera) {
+    for (let obj of this.objects) {
+      obj.render?.(camera);
+      obj.drawDebug?.(camera);
     }
-    render(camera) {
-        for (let obj of this.objects) {
-            obj.render?.(camera);
-            //obj.drawDebug?.(camera);
-        }
+  }
+  drawUI(camera) {
+    for (let ui of this.uiObjects) {
+      ui.drawText?.(camera);
     }
-    drawUI(camera) {
-        for (let ui of this.uiObjects) {
-            ui.drawText?.(camera);
-        }
-    }
-    onEnter() {}
-    onExit() {}
+  }
+  onEnter() {}
+  onExit() {}
 }
 export default Scene;
